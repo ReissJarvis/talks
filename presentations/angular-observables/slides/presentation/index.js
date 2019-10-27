e4d293b247d6ef9a5acb59ed6e785c535a16407b
Reiss Jarvis <reissefer@gmail.com>
2019-10-27 12:21:15 +0000

    add code example and update boilerplate slide code

---
+++ presentations/angular-observables/slides/presentation/index.js
@@ -0,0 +1,128 @@
+// Import React
+import React from 'react';
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Notes,
+  Quote,
+  Slide,
+  Text
+} from 'spectacle';
+
+import CodeSlide from 'spectacle-code-slide';
+
+// Import theme
+import createTheme from 'spectacle/lib/themes/default';
+
+const images = {
+  formidagon: require('../assets/formidable-logo.svg'),
+  goodWork: require('../assets/good-work.gif')
+};
+
+// Require CSS
+require('normalize.css');
+
+const theme = createTheme(
+    {
+      primary: '#757575',
+      secondary: '#ff5722',
+      tertiary: '#494949',
+      quartenary: '#122b45',
+    },
+    {
+      primary: 'Helvetica',
+      secondary: {
+        name: 'Droid Serif',
+        googleFont: true,
+        styles: ['400', '700i']
+      }
+    }
+);
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck
+        transition={['zoom', 'slide']}
+        transitionDuration={500}
+        progress="bar"
+        theme={theme}
+      >
+        <CodeSlide
+            transition={[]}
+            lang="js"
+            code={require("raw-loader!../assets/code-test.ts")}
+            ranges={[
+              { loc: [0, 11], title: "Walking through some code" },
+              { loc: [0, 1], title: "The Beginning" },
+              { loc: [1, 2], note: "Heres a note!" },
+              { loc: [2, 3] },
+              { loc: [7, 8], title: "Decleration"},
+              { loc: [8, 9] },
+            ]}/>
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Spectacle Boilerplate
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" fit bold>
+            open the presentation/index.js file to get started
+          </Text>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Image src={images.formidagon} width={800} />
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>
+            Typography
+          </Heading>
+          <Heading size={1} textColor="secondary">
+            Heading 1
+          </Heading>
+          <Heading size={2} textColor="secondary">
+            Heading 2
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            Heading 3
+          </Heading>
+          <Heading size={4} textColor="secondary">
+            Heading 4
+          </Heading>
+          <Heading size={5} textColor="secondary">
+            Heading 5
+          </Heading>
+          <Text size={6} textColor="secondary">
+            Standard text
+          </Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
+          <List>
+            <ListItem bulletStyle="star">Item 1</ListItem>
+            <ListItem bulletStyle="cross">Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite margin="10px 0 0 30px">Author</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide>
+          <Image src={images.goodWork} width={500} />
+          <Notes>gifs work too</Notes>
+        </Slide>
+      </Deck>
+    );
+  }
+}
